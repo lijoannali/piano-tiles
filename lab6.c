17f9dc03a9ab8cc8b79e6f78c6503d576e23a29a
Joanna Li <joannali.jli@gmail.com>
2026-04-07 21:12:15 -0500

    We successfully sent SPI from PA9 PICO to the array. it did not light up red as intended, oops

--- lab6.c
+++ lab6.c
@@ -7,12 +7,12 @@
 #include "leds.h"
 // #include "leds_poll.h"
 
-#define NUM_LEDS      256
-#define BYTES_PER_LED 9
+// #define NUM_LEDS      256
+// #define BYTES_PER_LED 9
 
-uint8_t buf[NUM_LEDS * BYTES_PER_LED];  // 2304 bytes, global = safe
+// uint8_t buf[NUM_LEDS * BYTES_PER_LED];  // 2304 bytes, global = safe
 
-uint8_t rgb_buf[NUM_LEDS * 3];  // ← was uint8_t buf[NUM_LEDS * 9]
+// uint8_t rgb_buf[NUM_LEDS * 3];  // ← was uint8_t buf[NUM_LEDS * 9]
 
 // int main(void) {
 //     InitializeLEDInterface_Poll();          // ← was InitializeLEDInterface()
@@ -33,6 +33,21 @@ uint8_t rgb_buf[NUM_LEDS * 3];  // ← was uint8_t buf[NUM_LEDS * 9]
 //     while(1);
 // }
 
+uint8_t test_packet[] = {0xAA, 0x55, 0xFF, 0x00};
+uint8_t message_len = 4;
+
+//Test SPI message!
+#define red1 0b11011011
+#define red2 0b01101101
+#define red3 0b10110110
+
+// red = 110 110 110 110 110 110 110 110
+// = 11011011 01101101 10110110
+
+// this array has 6 red RGB codes, each red code is the sequence: red1,red2,red3
+uint8_t red[] = {red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3};
+uint8_t red_length = sizeof(red)/sizeof(red[0]); 
+
 int main(void) {
     InitializeLEDInterface();
     delay_cycles(16000);
@@ -43,10 +58,26 @@ int main(void) {
     GPIOB->DOESET31_0 = (1 << 8);
     GPIOB->DOUT31_0 |= (1 << 8);
 
-
     GPIOB->DOESET31_0 = (1 << 15);
     GPIOB->DOUT31_0 |= (1 << 15);
-    while(1);
+
+    while (1) {
+        while (!SendSPIMessage(red, red_length)) {
+            // wait for previous to finish
+        }
+        delay_cycles(1000000); // gap between bursts
+    }
+
+
+
+    // while (!SendSPIMessage(onTxPacket, message_len)) 
+    // {
+    //     // Block until previous message is complete                
+    // }
+    //Test that your PA9 is working
+    // GPIOA->DOESET31_0 = (1 << 9);
+    // GPIOA->DOUT31_0 |= (1 << 9);
+    // while(1);
 }
 
 
