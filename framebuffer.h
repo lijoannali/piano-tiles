5c75b440b0ec3f989039ec087ded11c1e5495cc6
xiaoxi Luo <joannali.jli@gmail.com>
2026-04-15 21:23:07 -0500

    Add falling rectangles example

--- framebuffer.h
+++ framebuffer.h
@@ -3,13 +3,15 @@
 
 #include <stdint.h>
 
-#define COLOR_OFF     0x00
-#define COLOR_DIM_RED 0x01
-#define COLOR_DIM_BLUE 0x10
-#define COLOR_DIM_GREEN 0x11
+#define COLOR_OFF       0x00
+#define COLOR_DIM_RED   0x01
+#define COLOR_DIM_GREEN 0x02
+#define COLOR_DIM_BLUE  0x03
 
 void ClearFramebuffer(void);
 void DrawRow(int row, uint8_t color);
+void DrawVerticalSegment(int row, uint8_t color, int start_col, int length);
+void DrawRectangle(int x, int y, int h, int w, uint8_t color);
 void FlushFramebuffer(void);
 
 #endif
--- lab6.c
+++ lab6.c
@@ -1,102 +1,50 @@
 #include <ti/devices/msp/msp.h>
-// #include</Users/joanna/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/m0p/mspm0g350x.h>
 #include "delay.h"
-#include "buttons.h"
 #include "timing.h"
-#include "buzzer.h"
 #include "leds.h"
-// #include "leds_poll.h"
+#include "framebuffer.h"
 
-// #define NUM_LEDS      256
-// #define BYTES_PER_LED 9
+int main(void)
+{
+    int cycle  = 0;
 
-// uint8_t buf[NUM_LEDS * BYTES_PER_LED];  // 2304 bytes, global = safe
-
-// uint8_t rgb_buf[NUM_LEDS * 3];  // ← was uint8_t buf[NUM_LEDS * 9]
-
-// int main(void) {
-//     InitializeLEDInterface_Poll();          // ← was InitializeLEDInterface()
-
-//     FillStrip_Poll(rgb_buf, NUM_LEDS, 50, 0, 0);  // ← replaces the for loop + SendSPIMessage
-
-//     while(1);
-// }
-// int main(void) {
-//     InitializeLEDInterface();
-
-//     for (int i = 0; i < NUM_LEDS; i++) {
-//         RGB_2_SPI(&buf[i * BYTES_PER_LED], 50, 0, 0);  // RED
-//     }
-
-//     SendSPIMessage(buf, NUM_LEDS * BYTES_PER_LED);
-
-//     while(1);
-// }
-
-uint8_t test_packet[] = {0xAA, 0x55, 0xFF, 0x00};
-uint8_t message_len = 4;
-
-//Test SPI message!
-
-// red = 110 110 110 110 110 110 110 110
-// = 11011011 01101101 10110110
-
-// this array has 6 red RGB codes, each red code is the sequence: red1,red2,red3
-// uint8_t red[] = {red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3, red1, red2, red3};
-// uint8_t red_length = sizeof(red)/sizeof(red[0]); 
-#define red1 0b11011011
-#define red2 0b01101101
-#define red3 0b10110110
-#define zero1 0b10010010
-#define zero2 0b01001001
-#define zero3 0b00100100
-#define reset 0b00000000
-
-// red = 110 110 110 110 110 110 110 110
-// = 11011011 01101101 10110110
-
-// this array has 6 red RGB codes, each red code is the sequence: zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3
-uint8_t red[] = {
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3,
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3,
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3,
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3,
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3,
-    zero1, zero2, zero3, red1, red2, red3, zero1, zero2, zero3
-};
-uint8_t red_length = sizeof(red)/sizeof(red[0]); 
-
-
-int main(void) {
     InitializeLEDInterface();
-    delay_cycles(16000);
-    GPIOA->DOESET31_0 = (1 << 26);
-    GPIOA->DOUT31_0 |= (1 << 26);
+    InitializeTimerG0();
 
-    //We want to use the SPI on this pin!
-    GPIOB->DOESET31_0 = (1 << 8);
-    GPIOB->DOUT31_0 |= (1 << 8);
-
-    GPIOB->DOESET31_0 = (1 << 15);
-    GPIOB->DOUT31_0 |= (1 << 15);
-
-    while (1) {
-        while (!SendSPIMessage(red, 27)) {
-            // wait for previous to finish
-        }
-        delay_cycles(1000000); // gap between bursts
+    if (GPIOA->GPRCM.STAT & GPIO_STAT_RESETSTKY_MASK) {
+        // Sticky bit is set  so module was reset (or never initialized)
+        // Do full reset sequence and clear the sticky bit
+        GPIOA->GPRCM.RSTCTL = (GPIO_RSTCTL_KEY_UNLOCK_W |
+                                GPIO_RSTCTL_RESETSTKYCLR_CLR |
+                                GPIO_RSTCTL_RESETASSERT_ASSERT);
+        GPIOA->GPRCM.PWREN  = (GPIO_PWREN_KEY_UNLOCK_W |
+                                GPIO_PWREN_ENABLE_ENABLE);
+        delay_cycles(POWER_STARTUP_DELAY);
     }
 
+    IOMUX->SECCFG.PINCM[(IOMUX_PINCM1)] = IOMUX_PINCM_PC_CONNECTED | 1;  // LED GPIO
+    GPIOA->DOUT31_0 = 1;
+    GPIOA->DOE31_0 = 1;
 
+    // let the buzzer run for 0.1 s just so we know it's there!
+    delay_cycles(1600000);
 
-    // while (!SendSPIMessage(onTxPacket, message_len)) 
-    // {
-    //     // Block until previous message is complete                
-    // }
-    //Test that your PA9 is working
-    // GPIOA->DOESET31_0 = (1 << 9);
-    // GPIOA->DOUT31_0 |= (1 << 9);
-    // while(1);
+    SetTimerG0Delay(10000); // 20 ticks at 32 kHz is 0.6 ms
+    EnableTimerG0();
+
+    // VERY BASIC LOOP - If button 1 signals a 0, enable the PWM
+    int row = 2;
+    int start_col = -7;
+    while (1) {
+        if (timer_wakeup) {
+            ClearFramebuffer();
+            DrawRectangle(7, (start_col++)%16, 3,2, COLOR_DIM_RED);
+            DrawRectangle(3, (start_col++)%16, 3,2, COLOR_DIM_GREEN);
+//            DrawRectangle(10, (start_col++)%16, 3,2, COLOR_DIM_BLUE);
+            FlushFramebuffer();
+            timer_wakeup = 0;
+        }
+        __WFI(); // Go to sleep until timer counts down again.
+    }
+
 }
-
-
